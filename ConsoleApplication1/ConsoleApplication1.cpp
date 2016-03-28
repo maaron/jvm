050e0cb52f3de3ddf7a042f0a7059b5d607531db
maaron <m_aaron@yahoo.com>
2016-03-28 07:37:19 -0500

    Removed some unused test code

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -14,29 +14,13 @@ int main()
 		java::load_jvmdll("C:\\Program Files (x86)\\Java\\jdk1.8.0_25\\jre\\bin\\server\\jvm.dll");
 
 		java::vm_args args;
-		args.add_option("-Djava.class.path=C:/Users/maaron/Source/Repos/jvm/ConsoleApplication1/foo.jar");
+		//args.add_option("-Djava.class.path=C:/Users/maaron/Source/Repos/jvm/ConsoleApplication1/foo.jar");
 		//args.add_option("-verbose");
 		args.ignore_unrecognized(false);
 		java::vm jvm(args);
 
 		try
-		{
-#if 0
-			nih.make_global();
-			nih.call("getConstructors");
-
-			auto ctors = nih.get_constructors();
-			for (auto ctor : ctors)
-			{
-				std::cout << "ctor name: " << ctor.name() << std::endl;
-			}
-			
-			auto methods = nih.get_methods();
-			for (auto m : methods)
-			{
-				std::cout << "method name: " << m.name() << std::endl;
-			}
-#endif		
+		{	
 			java::clazz comparableIface("java/lang/Comparable");
 
 			auto proxy = java::create_proxy(comparableIface, [](java::method m, java::object args) -> java::object
@@ -47,20 +31,6 @@ int main()
 
 			auto compare_result = proxy.call("compareTo", java::create("java/lang/Object"));
 			std::cout << "compareTo result is " << compare_result.as_int() << std::endl;
-
-			auto a = java::create("foo/A");
-			std::vector<java::clazz> classes;
-			classes.push_back(java::object("asdf").get_clazz());
-
-			auto i_class = java::clazz("foo/I");
-			
-			auto a_method = a.get_clazz().lookup_method("m", classes);
-			auto i_method = i_class.lookup_method("m", classes);
-
-			auto i_result = java::jni::call_method<jint>(a.native(), i_method.id(), java::object("asdf").native());
-			auto a_result = java::jni::call_method<jint>(a.native(), a_method.id(), java::object("asdf").native());
-
-			auto result = a.call("m", "asdf").as_int();
 		}
 		catch (java::exception& je)
 		{
